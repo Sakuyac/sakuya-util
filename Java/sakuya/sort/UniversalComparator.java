3237c3371fce40d54d9a52d5f7f653b54f0885c3
chenyuanqiao <18770052131@163.com>
2021-04-22 11:34:43 +0800

    上传排序工具类

---
+++ Java/sakuya/sort/UniversalComparator.java
@@ -0,0 +1,56 @@
+package sakuya.sort;
+
+import java.util.List;
+
+/**
+ * @author chenyuanqiao
+ * @date 2021/3/24
+ */
+public abstract class UniversalComparator<T, K> {
+
+    /**
+     * 智能排序
+     * @param o1List 需要排序的对象列表
+     * @param o2List 排序字段列表
+     * */
+    public void sort(List<T> o1List, List<K> o2List){
+        int count = o1List.size();
+        int end = o1List.size() - 1;
+        int i = 0;
+        while (count > 1){
+            T o1 = o1List.get(i);
+            for (int j = 0 ; j < o2List.size() ; j++){
+                if (compare(o1, o2List.get(j))){
+                    if (i == j){
+                        i++;
+                    } else {
+                        swap(i, j, o1List);
+                    }
+                    count--;
+                    break;
+                }
+                if (j == o2List.size() - 1){
+                    swap(i, end--, o1List);
+                    count--;
+                }
+
+            }
+        }
+    }
+    /**
+     * 交换列表中属性
+     * */
+    public void swap(Integer index1, Integer index2, List<T> o1List){
+        T o1 = o1List.get(index1);
+        o1List.set(index1, o1List.get(index2));
+        o1List.set(index2, o1);
+    }
+
+    /**
+     * 返回是否相等
+     * @param o1 对象
+     * @param o2 对象
+     * @return true false
+     * */
+    public abstract boolean compare(T o1, K o2);
+}
